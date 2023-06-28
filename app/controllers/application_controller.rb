856488a1f66ea29244e4b3ea54411ae300756c8a
Nil Patel <125335891+nilp06@users.noreply.github.com>
2023-06-28 14:50:15 +0530

    refactor :  update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :user_signed_in?
-  helper_method :current_user
+  helper_method %i[user_signed_in?, current_user]
   before_action :authenticate_user
 
   def current_user
